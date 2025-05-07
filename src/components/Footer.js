8e2d871b9ae15629f840f4dd1a01335d9f8778aa
olexii yevdoshchenko <olexiyewdoshchenko@gmail.com>
2025-05-07 09:41:48 +0300

    Додав зображення та їхній опис

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {                                          // Створення функціонального компонента Footer
     return (                                                   // Повертає JSX
         <footer className="footer">                             {/*Головний елемент футера*/}
-            <p>&copy; 2023 My Website</p>                       {/*Текст з авторським правом*/}
+            <p>&copy; 2025 </p>                       {/*Текст з авторським правом*/}
             <ul>                                               {/*Список соціальних мереж*/}
                 <li><a href="#facebook">Facebook</a></li>      {/*Посилання на Facebook*/}
                 <li><a href="#twitter">Twitter</a></li>        {/*Посилання на Twitter*/}
