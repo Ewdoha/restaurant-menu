4aa08f5fb511f8f16227555a167cbd654d10d92f
olexii yevdoshchenko <olexiyewdoshchenko@gmail.com>
2025-05-26 16:30:07 +0300

    Додав позиції у fishDishes

--- src/data/fishDishes.js
+++ src/data/fishDishes.js
@@ -1,11 +1,120 @@
 const fishDishes = [
 
-    { name: "Запечений морський окунь з овочевим соте", 
+    {   name: "Сібас(філе) з різотто, шпинатом, зеленим горошком та лисичками", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 350, 
+        price: 380, 
+        image: "",
+        ingredients: '' 
+    },
+
+    {   name: "Скумбрія фарширована", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 100, 
+        price: 60, 
+        image: "",
+        ingredients: '(печериці, цибуля ріпчаста, морква, сирок плавлений, зелень, лимон)' 
+    },
+
+    {   name: "Скумбрія по сицилійські", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 220, 
+        price: 280, 
+        image: "",
+        ingredients: '(скумбрія, помідори, печериці, перець болгарський, зелень)' 
+    },
+
+    {   name: "Запечений морський окунь з овочевим соте", 
         category: 'Страви з риби', 
         unit: ' г', 
         amount: 100, 
         price: 180, 
-        image: "/images/Zapechenyi_morskyi_okun_z_ovochevym_sote.jpg" 
+        image: "/images/Zapechenyi_morskyi_okun_z_ovochevym_sote.jpg",
+        ingredients: '' 
+    },
+
+    {   name: "Філе судака смажене, в сухарях з пармезаном", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: '160/30', 
+        price: 250, 
+        image: "",
+        ingredients: '' 
+    },
+
+    {   name: "Філе судака в кунжуті з вершково-сирним соусом", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: '180/50', 
+        price: 250, 
+        image: "",
+        ingredients: '' 
+    },
+
+    {   name: "Філе судака з печерицями під сиром", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 180, 
+        price: 220, 
+        image: "",
+        ingredients: '' 
+    },
+
+    {   name: "Соте з сьомги та судака з овочами під вершковим соусом", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 350, 
+        price: 390, 
+        image: "",
+        ingredients: '' 
+    },
+
+    {   name: "Лосось на подушці з птитиму", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: '100/150', 
+        price: 320, 
+        image: "",
+        ingredients: '' 
+    },
+
+    {   name: "Стейк із сьомги гриль", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 'за 100', 
+        price: 130, 
+        image: "",
+        ingredients: '' 
+    },
+
+    {   name: "Філе сьомги з соусом тартар гриль", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 'за 100', 
+        price: 150, 
+        image: "",
+        ingredients: '(сьомга, соус тартар, лимон, листя салату, зелень)' 
+    },
+
+    {   name: "Філе сьомги у вершково-ікорному соусі", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 'за 100', 
+        price: 150, 
+        image: "",
+        ingredients: '(сьомга, вершки, ікра червона, зелень)' 
+    },
+
+    {   name: "Форель з карамелізованим апельсином", 
+        category: 'Страви з риби', 
+        unit: ' г', 
+        amount: 'за 100', 
+        price: 76, 
+        image: "",
+        ingredients: '' 
     },
 
 ];
