0d8bfae45039abca14ecb4f0542514c81bc09678
olexii yevdoshchenko <olexiyewdoshchenko@gmail.com>
2025-05-26 20:34:26 +0300

    Додав позиції до desserts

--- src/data/desserts.js
+++ src/data/desserts.js
@@ -1,12 +1,111 @@
 const desserts = [
 
-    { name: "Млинці Буржуй", 
+    {   name: "Млинці «Малібу»", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: '170/10', 
+        price: 200, 
+        image: "",
+        ingredients: ''  
+    },
+
+    {   name: "Млинці з шоколадним кремом «Nutella»", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: '170/15/15', 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+
+    {   name: "Млинці з сиром та родзинками", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 200, 
+        price: 160, 
+        image: "",
+        ingredients: ''  
+    },
+
+    {   name: "Млинці з вишнями та маком", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 200, 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+    
+    { name: "Млинці вишиванка", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 220, 
+        price: 220, 
+        image: "/images/Mlyntsi_Vyshyvanka.jpg",
+        ingredients: '(маскарпоне, мак, вишня)' 
+    },
+
+    {   name: "Млинці з вишнями та вершками", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: '220/50', 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+
+    {   name: "Млинці шоколадні з маскарпоне і фініками", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 220, 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+
+    { name: "Фірмові шоколадні млинці «Буржуй»", 
         category: 'Десерти', 
         unit: ' г', 
         amount: 190, 
-        price: 160, 
+        price: 180, 
         image: "/images/Mlyntsi_Burzhui.jpg",
         ingredients: '(вершковий сир, желе)'  
+    },
+
+    {   name: "Млинці з маскарпоне і малиною", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 220, 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+
+    {   name: "Млинці з яблуками із заварним кремом", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 220, 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+
+    {   name: "Млинці шоколадні з сиром та чорницею", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 260, 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+
+    {   name: "Млинці Снікерс", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 200, 
+        price: 180, 
+        image: "",
+        ingredients: '(сир філадельфія, згущене молоко, арахіс, грецький горіх)'  
     },
 
     { name: "Млинці «Весільні»", 
@@ -18,14 +117,25 @@ const desserts = [
         ingredients: '(творог домашній, кориця, цукор, вершки)'  
     },
 
-    { name: "Млинці вишиванка", 
+    {   name: "Асорті сухофруктів з вершками", 
         category: 'Десерти', 
         unit: ' г', 
-        amount: 220, 
+        amount: 140, 
         price: 200, 
-        image: "/images/Mlyntsi_Vyshyvanka.jpg",
-        ingredients: '(маскарпоне, мак, вишня)' 
+        image: "",
+        ingredients: '(чорнослив, фініки та курага)'  
     },
+
+    {   name: "Чорнослив з вершками", 
+        category: 'Десерти', 
+        unit: ' г', 
+        amount: 140, 
+        price: 180, 
+        image: "",
+        ingredients: ''  
+    },
+
+    
 
 ];
 
